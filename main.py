e90de44dc787139d1e7802300ae90fff77603f59
RKuhl <ryanakuhl@gmail.com>
2019-05-20 14:29:55 -0400

    Update main.py
    Added child class changes
    Removes empty tags

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from pprint import pprint
-from bs4 import BeautifulSoup
 import re, requests, pyperclip
-
+from bs4 import BeautifulSoup
 
 three_to_four = {'Bootstrap 3x': 'Bootstrap 4',
                  'col-\d-offset-\d': 'col offset-\d',
@@ -51,25 +50,21 @@ three_to_four = {'Bootstrap 3x': 'Bootstrap 4',
                  'badge': 'badge badge-pill'
                  }
 
-"""
-??? Not yet sure how to move these
-'list-inline > li': 'list-inline-item',
-'dropdown-menu > li': 'dropdown-item',
-'nav navbar > li': 'nav-item',
-'nav navbar > li > a': 'nav-link',
-'pagination > li': 'page-item',
-'pagination > li > a': 'page-link',
-"""
+three_to_four_list = {
+                      'list-inline': 'list-inline-item',
+                      'dropdown-menu': 'dropdown-item',
+                      'navbar': 'nav-item',
+                      'pagination': 'page-item',
+                      }
 
 
-def sanitize_html(all_text):
-    all_text = all_text.replace('<b>', '<strong>').replace('</b>', '</strong>').replace('<i>', '<em>')
-    all_text = all_text.replace('<b></b>', '').replace('<span></span>', '').replace('<ul></ul>', '')
-    all_text = all_text.replace('<li></li>', '').replace('</i>', '</em>')
-    all_text = BeautifulSoup(all_text, 'html.parser')
-    all_text = all_text.prettify()
-    pyperclip.copy(all_text)
-    return all_text
+def sanitize_html(all_text_in):
+  all_text = all_text_in.replace('<b>', '<strong>').replace('</b>', '</strong>')
+  all_text = all_text.replace('<i>', '<em>').replace('</i>', '</em>')
+  all_text = BeautifulSoup(all_text, 'html.parser')
+  empty_tags = all_text.findAll(lambda tag: not tag.contents)
+  [empty_tag.extract() for empty_tag in empty_tags]
+  pyperclip.copy(all_text.prettify())
 
 
 def iter_dict(a):
@@ -80,9 +75,34 @@ def iter_dict(a):
     return aa
 
 
+def list_items(a):
+  soup = BeautifulSoup(changing_html, 'html.parser')
+  for kid in soup.find(class_=a).children:
+    if kid.name == 'li':
+      if kid.has_attr('class'):
+        kid['class'] = kid['class'] + ' ' + three_to_four_list.get(a)
+      else:
+        kid['class'] = three_to_four_list.get(a)
+      if a == 'navbar' or 'pagination':
+        for nested_link in kid.find_all('a'):
+          if nested_link.has_attr('class'):
+            nested_link['class'] = kid['class'] + ' ' + three_to_four_list.get(a).replace('item', 'link')
+          else:
+            nested_link['class'] = three_to_four_list.get(a).replace('item', 'link')
+  soup.find(class_=a)['class'] = ''
+  return str(soup).replace(' class=""', '')
+
+
 changing_html = pyperclip.paste()
 regexpHandler = re.findall('class="(.*?)"', changing_html)
-for class_to_change in regexpHandler:
-    changing_html = iter_dict(class_to_change)
+list = []
+for r in regexpHandler:
+  for a in r.split(' '):
+    if a not in list:
+      list.append(a)
+for class_to_change in list:
+  if class_to_change in three_to_four_list:
+      changing_html = list_items(class_to_change)
+  elif class_to_change in three_to_four:
+      changing_html = iter_dict(class_to_change)
 sanitize_html(changing_html)
-
